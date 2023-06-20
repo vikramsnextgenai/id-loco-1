83f5fe82666c32800a3e920abef652f417dc294a
vikramsnextgenai <>
2023-06-20 10:25:12 +0000

    Updates

---
+++ _next/static/YO-kty2zjGwH0b_1rpWhl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-46bb1fed4e054686.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/speakers":["static/chunks/pages/speakers-5716e4fed4c54688.js"],sortedPages:["/","/_app","/_error","/speakers"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
